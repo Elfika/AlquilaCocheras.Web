19a3bc51f81d222c178e3999575c45a36c5fb0af
Loly <lorena.lgodoy@gmail.com>
2016-09-26 02:04:48 -0300

    first
    test

---
+++ js/scriptsGeneral.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    $("#txtFechaInicio").datepicker();
+    $("#txtFechaFin").datepicker();
+    $.datepicker.regional['es'] = {
+        monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+   'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+        dateFormat: 'dd/mm/yy', firstDay: 0,
+        dayNames: ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'],
+        dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'],
+        dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa']
+    };
+    $.datepicker.setDefaults($.datepicker.regional['es']);
+
+    
+    $('#txtHorarioFin').daterangepicker({
+        "timePicker": true,
+        "timePicker24Hour": true,
+        "timePickerIncrement": 30,
+        "showCustomRangeLabel": false
+
+
+    });
+});
+
